8ece24d05e24b7083057efb993c36e00943b0b6b
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-08-29 10:45:12 +0200

    Using component with TS import

---
+++ src/components/mapView/MapView.ts
@@ -0,0 +1,8 @@
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Map View",
+  setup() {
+    return {};
+  },
+});
---
+++ src/components/mapView/MapView.vue
@@ -0,0 +1,7 @@
+<script lang="ts" src="./MapView.ts"/>
+
+<template>
+  <h1>Map View</h1>
+</template>
+
+<style scoped></style>
