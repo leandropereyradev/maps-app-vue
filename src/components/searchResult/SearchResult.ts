766ecebc11afb825fa4710fe96d649c87ce723a3
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-08-29 20:15:04 +0200

    Activate and move to the selected place

--- src/components/searchResult/SearchResult.ts
+++ src/components/searchResult/SearchResult.ts
@@ -1,14 +1,29 @@
-import { defineComponent } from "vue";
-import { usePlacesStore } from "@/composables";
+import { defineComponent, ref } from "vue";
+import { useMapStore, usePlacesStore } from "@/composables";
+import { Feature } from "@/interfaces/places";
 
 export default defineComponent({
   name: "Search Result",
   setup() {
     const { places, isLoadingPlaces } = usePlacesStore();
+    const { map } = useMapStore();
+    const activePlace = ref("");
 
     return {
       isLoadingPlaces,
       places,
+      activePlace,
+
+      onPlaceClicked: (place: Feature) => {
+        activePlace.value = place.id;
+
+        const [lng, lat] = place.center;
+
+        map.value?.flyTo({
+          zoom: 15,
+          center: [lng, lat],
+        });
+      },
     };
   },
 });
